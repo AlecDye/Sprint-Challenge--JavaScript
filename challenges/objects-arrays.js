64eda1b9dba9713d456b0bf0b40235b9a8576ffa
Alec Dye <alecdye@gmail.com>
2019-12-20 08:26:02 -0800

    Rawr!

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -18,6 +18,8 @@ const dinosaurOne = {
   weight: "7000kg",
   length: "12m",
   period: "Late Cretaceous",
+  //added sounded
+  sound: "RAWERSRARARWERSARARARRRR!",
 };
 
 const dinosaurTwo = {
@@ -39,20 +41,20 @@ const dinosaurThree = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinosaurOne.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinosaurThree.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinosaurTwo.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinosaurOne.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(dinosaurOne.sound);
 
 
 // ==== Arrays ====
